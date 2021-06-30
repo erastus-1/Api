10a8bd433a5935303ce7c2f485a7a8e632f1b635
erastus-1 <erastuskariuki15@gmail.com>
2021-06-30 11:50:52 +0300

    added articles api

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,49 @@
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import generics
 from django.shortcuts import render
+from rest_framework import status
+from .serializers import *
+from .models import *
+import datetime as dt
 
-# Create your views here.
+#Create your views here.
+class ArticleView(generics.DestroyAPIView):
+    queryset= Article.objects.all()
+    serializer_class= ArticleSerializer
+    permission_classes = (AllowAny,)
+
+    def delete(self, request, pk, format=None):
+        article = self.get_object()
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ArticlePostView(generics.ListCreateAPIView):
+    queryset= Article.objects.all()
+    serializer_class= ArticleSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, format=None):  
+        serializers = ArticleSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request,pk):
+        try:
+            article = Article.objects.get(id=pk)
+        except:
+            article=None
+        if article:
+            serializer = ArticleSerializer(instance=article, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response("Post not found")
+
+    def get(self, request, format=None):
+        all_articles = Article.objects.all()
+        serializers = ArticleSerializer(all_articles , many=True)
+        return Response(serializers.data)
